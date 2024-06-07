3b210ca2598e33c9a8b2e45d4fb389a2eb9e8683
Tiago <tiago.cambara@visualsoft.co.uk>
2024-06-07 11:10:47 +0100

    feat(sc-146): add the GRCP setup

---
+++ src/setup/microservice.loader.ts
@@ -0,0 +1,34 @@
+import { INestMicroservice } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppContextModule } from '../app_context.module';
+import { HttpSetup } from './config/config_loader.interface';
+import { configLoaderEnum } from './config/config.loader';
+import { AppModule } from '../app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+export const loadMicroservice = async (): Promise<INestMicroservice> => {
+  // TODO: Remove when the following is fixed https://github.com/nestjs/nest/issues/2343
+  const appContext =
+    await NestFactory.createApplicationContext(AppContextModule);
+
+  // Get the host env vars
+  const configService = appContext.get<ConfigService>(ConfigService);
+  const { host, port } = configService.get<HttpSetup>(configLoaderEnum.HTTP);
+  appContext.close();
+
+  const protoPath = join(__dirname, '../domain/proto/pokedex.proto');
+
+  // Setup the microservice
+  const microservice =
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+      transport: Transport.GRPC,
+      options: {
+        url: `${host}:${port}`,
+        package: 'pokedex',
+        protoPath,
+      },
+    });
+  return microservice;
+};
