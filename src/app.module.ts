6221a54d489f6cccbc4cf0b65431a18b2e833baa
Tiago <tiago.cambara@visualsoft.co.uk>
2024-06-07 11:14:12 +0100

    feat(sc-146): import the ConfigModule and the HealthCheckModulo into the AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { HealthCheckModule } from './health_check/health_check.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule, HealthCheckModule],
 })
 export class AppModule {}
