e1d86f4813faaf10115bec6db9b619c45ce08e58
Tiago <tiago.cambara@visualsoft.co.uk>
2024-06-24 18:10:21 +0100

    feat(sc-175): implement the pagination module

---
+++ src/infra/pagination/helpers/pagination.helper.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@nestjs/common';
+import {
+  PaginationData,
+  PaginationDto,
+  PaginationMeta,
+} from '../models/pagination.model';
+
+interface MakePaginationOptions<Data> extends PaginationData<Data> {
+  limit?: number;
+  skip?: number;
+}
+
+type MakePaginationMetaOptions = Omit<MakePaginationOptions<unknown>, 'data'>;
+
+@Injectable()
+export class PaginationHelper {
+  makePaginationResponse<Data>(
+    options: MakePaginationOptions<Data>,
+  ): PaginationDto<Data> {
+    const { data, ...metaOptions } = options;
+    return {
+      data,
+      meta: this.makePaginationMeta(metaOptions),
+    };
+  }
+
+  private makePaginationMeta({
+    limit,
+    skip,
+    total,
+  }: MakePaginationMetaOptions): PaginationMeta {
+    if (!skip && !limit) {
+      return {
+        page: 1,
+        pageCount: 1,
+        itemCount: total,
+        hasNextPage: false,
+        hasPreviousPage: false,
+        take: total,
+      };
+    }
+    const limitValue = limit ?? total;
+    const skipValue = skip ?? 0;
+    const pageCount = Math.ceil(total / limitValue);
+    const currentPage = this.calculateCurrentPage(skipValue, total, limitValue);
+
+    return {
+      page: currentPage,
+      pageCount,
+      itemCount: total,
+      take: limitValue,
+      hasNextPage: currentPage < pageCount,
+      hasPreviousPage: currentPage > 1,
+    };
+  }
+
+  private calculateCurrentPage(
+    skip: number,
+    total: number,
+    limit: number,
+  ): number {
+    if (skip === 0) return 1;
+
+    return Math.ceil((skip * limit) / total);
+  }
+}
