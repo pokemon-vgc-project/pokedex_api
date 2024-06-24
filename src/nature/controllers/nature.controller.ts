d10c7cc65319d44b04b9f989bdb19e38ce484ebe
Tiago <tiago.cambara@visualsoft.co.uk>
2024-06-24 18:10:59 +0100

    feat(sc-175): return the pagination data on get natures EP

--- src/nature/controllers/nature.controller.ts
+++ src/nature/controllers/nature.controller.ts
@@ -3,13 +3,24 @@ import { pokedex } from '../../domain/proto/pokedex';
 import { GrpcMethod } from '@nestjs/microservices';
 import { Controller } from '@nestjs/common';
 import { NatureService } from '../services/nature.service';
+import { PaginationHelper } from 'src/infra/pagination/helpers/pagination.helper';
 
 @Controller()
 export class NatureController implements pokedex.NatureService {
-  constructor(private readonly natureService: NatureService) {}
+  constructor(
+    private readonly natureService: NatureService,
+    private readonly paginationHelper: PaginationHelper,
+  ) {}
 
   @GrpcMethod('NatureService', 'GetNatures')
   getNatures(): Observable<pokedex.ResponseNatureDto> {
-    return from(this.natureService.getList()).pipe(map((data) => ({ data })));
+    return from(this.natureService.getList()).pipe(
+      map(({ data, total }) =>
+        this.paginationHelper.makePaginationResponse({
+          data,
+          total,
+        }),
+      ),
+    );
   }
 }
