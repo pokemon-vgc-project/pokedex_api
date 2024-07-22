ae58fae0f183ddbfa404f9d79fb7e2133c9bc0b2
Tiago <tiago.cambara@visualsoft.co.uk>
2024-07-22 11:25:19 +0100

    feat(sc-202): implement the pokemon forms method and add the pokemon service into module

--- src/pokemon/controllers/pokemon.controller.ts
+++ src/pokemon/controllers/pokemon.controller.ts
@@ -8,11 +8,13 @@ import {
   PokemonServiceMethods,
 } from '@pokemon-vgc-project/lib-proto';
 import { pokedex } from 'src/domain/proto/@pokemon-vgc-project/lib-proto/proto/pokedex';
+import { PokemonService } from '../services/pokemon.service';
 
 @Controller()
 export class PokemonController implements pokedex.PokemonService {
   constructor(
     private readonly pokemonTypeService: PokemonTypeService,
+    private readonly pokemonService: PokemonService,
     private readonly paginationHelper: PaginationHelper,
   ) {}
 
@@ -26,6 +28,28 @@ export class PokemonController implements pokedex.PokemonService {
   }: pokedex.GetTypesOptions): Observable<pokedex.ResponsePokemonTypesDto> {
     return from(
       this.pokemonTypeService.getTypes({ pagination: { limit, skip } }),
+    ).pipe(
+      map(({ data, total }) =>
+        this.paginationHelper.makePaginationResponse({
+          data,
+          total,
+          limit,
+          skip,
+        }),
+      ),
+    );
+  }
+
+  @GrpcMethod(
+    PokedexServices.POKEMON_SERVICE,
+    PokemonServiceMethods.GET_POKEMON_FORMS,
+  )
+  getPokemonForms({
+    limit,
+    skip,
+  }: pokedex.GetPokemonFormsOptions): Observable<pokedex.ResponsePokemonFormsDto> {
+    return from(
+      this.pokemonService.getFormes({ pagination: { limit, skip } }),
     ).pipe(
       map(({ data, total }) =>
         this.paginationHelper.makePaginationResponse({
